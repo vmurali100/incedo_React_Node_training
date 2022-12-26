ea5f911266371ecfaf8af8133a9a4e6b5e7eb8d8
vmurali100 <vmurali100@users.noreply.github.com>
2022-12-26 13:26:18 +0530

    Added

--- ReactJS/app2-class-comps/src/App.js
+++ ReactJS/app2-class-comps/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import Parent from './Components/Parent';
 import { Person } from './Components/Person';
 import User, { MainUser, OtherUser } from './Components/User'
 
 function App() {
   return (
     <div className="App">
-      <Person/>
+      {/* <Person/> */}
       {/* <User/>
       <MainUser/>
       <OtherUser/> */}
+      <Parent/>
     </div>
   );
 }
