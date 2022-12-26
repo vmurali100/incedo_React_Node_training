b6f9def826148748c6701c69f7ee1131639d0757
vmurali100 <vmurali100@users.noreply.github.com>
2022-12-26 12:32:28 +0530

    Object And arrays

--- ReactJS/app2-class-comps/src/Components/Person.jsx
+++ ReactJS/app2-class-comps/src/Components/Person.jsx
@@ -8,15 +8,28 @@ export class Person extends Component {
       personDetails: {
         email: "ram@gmail.com",
         city: "Bangalore",
+        stte:"Karnataka",
+        langulage:"Kannada"
       },
-      subjects: ["HTML", "CSS", "JS", "ReactJS"],
+      subjects: ["HTML", "CSS", "JS", "ReactJS","NodeJS","MongoDB","Express"],
     };
   }
   render() {
     return (
       <div>
-        <h2>{this.state.fname}</h2>
-        <h2>{this.state.personDetails.email}</h2>
+        {/* <h2>{this.state.fname}</h2>
+        <h2>{this.state.personDetails.email}</h2> */}
+        {/* <ul>
+            {this.state.subjects.map((val,i)=>{
+                return <li key={i}>{val}</li>
+            })}
+        </ul> */}
+
+        <ul>
+            {Object.keys(this.state.personDetails).map((val)=>{
+                return <li>{this.state.personDetails[val]}</li>
+            })}
+        </ul>
       </div>
     );
   }
