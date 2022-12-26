878f7288f46507eb391a95e5348bdfb5e8a46155
vmurali100 <vmurali100@users.noreply.github.com>
2022-12-26 12:56:24 +0530

    Event hanlders

--- ReactJS/app2-class-comps/src/Components/Person.jsx
+++ ReactJS/app2-class-comps/src/Components/Person.jsx
@@ -4,19 +4,41 @@ export class Person extends Component {
   constructor() {
     super();
     this.state = {
-      fname: "Murali",
+      isShowMesagge: false,
+      WelcomeMessage:
+        "Hi All ... Welcome to Incedeo ReactJS And NodeJS Training !!",
       personDetails: {
         email: "ram@gmail.com",
         city: "Bangalore",
-        stte:"Karnataka",
-        langulage:"Kannada"
+        stte: "Karnataka",
+        langulage: "Kannada",
       },
-      subjects: ["HTML", "CSS", "JS", "ReactJS","NodeJS","MongoDB","Express"],
+      subjects: [
+        "HTML",
+        "CSS",
+        "JS",
+        "ReactJS",
+        "NodeJS",
+        "MongoDB",
+        "Express",
+      ],
     };
+    this.showMessage = this.showMessage.bind(this);
   }
+  showMessage() {
+    // this.state.isShowMesagge = true
+    this.setState({ isShowMesagge: true });
+  }
+
+  // showMessage = ()=>{
+  //     this.setState({isShowMesagge:true})
+  // }
   render() {
+    const { WelcomeMessage } = this.state;
     return (
       <div>
+        <button onClick={this.showMessage}>Show Message </button>
+        {this.state.isShowMesagge && <h2>{WelcomeMessage}</h2>}
         {/* <h2>{this.state.fname}</h2>
         <h2>{this.state.personDetails.email}</h2> */}
         {/* <ul>
@@ -25,11 +47,11 @@ export class Person extends Component {
             })}
         </ul> */}
 
-        <ul>
+        {/* <ul>
             {Object.keys(this.state.personDetails).map((val)=>{
                 return <li>{this.state.personDetails[val]}</li>
             })}
-        </ul>
+        </ul> */}
       </div>
     );
   }
