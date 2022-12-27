e36f7b8d5b6010bdc21cd921186a1cea76384069
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2022-12-27 11:08:53 +0530

    ganesh

---
+++ React Js/app3-react-forms/src/App.js
@@ -0,0 +1,13 @@
+import User from './Components/User';
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
