76da7ff1821546da3cd0f3248ee1b6c9179dba6c
Suchi465 <107851318+Suchi465@users.noreply.github.com>
2022-12-27 11:12:12 +0530

    DAY-3

---
+++ ReactJS/app3-react-forms/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import User from "./Components/User";
+import { Register } from "./Components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <User /> */}
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/app3-react-forms/src/Components/Register.jsx
@@ -0,0 +1,97 @@
+import { Component } from "react";
+
+export class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      username: "",
+      password: "",
+      confirmpassword: "",
+    };
+  }
+  handleChange = (e) => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  };
+  register = () => {
+    console.log(this.state);
+    this.clearForm();
+  };
+  clearForm = () => {
+    this.setState ({
+      fname: "",
+      lname: "",
+      email: "",
+      username: "",
+      password: "",
+      confirmpassword: "",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="First Name">First Name : </label>
+          <br />
+          <input
+            type="text"
+            name="fname"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.fname}
+          />
+          <br />
+          <label htmlFor="Last Name">Last Name : </label>
+          <br />
+          <input
+            type="text"
+            name="lname"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.lname}
+          />
+          <br />
+          <label htmlFor="Email">Email Id : </label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.email}
+          />
+          <br />
+          <label htmlFor="User Name">User Name : </label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.username}
+          />
+          <br />
+          <label htmlFor="Password">Password : </label>
+          <br />
+          <input
+            type="text"
+            name="password"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.password}
+          />
+          <br />
+          <label htmlFor="Confirm Password">Confirm Password: </label>
+          <br />
+          <input
+            type="text"
+            name="confirmpassword"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.confirmpassword}
+          />
+          <br />
+          <button onClick={this.register} type="button">
+            Register
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
