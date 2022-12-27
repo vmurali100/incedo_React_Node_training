b2f30950c9a90528c9d2d320de76b318e255ca3e
reddyrups <121149033+reddyrups@users.noreply.github.com>
2022-12-27 12:10:30 +0530

    rup

--- ReactJS/rupapp3forms/src/Components/Forms.jsx
+++ ReactJS/rupapp3forms/src/Components/Forms.jsx
@@ -1,68 +1,84 @@
+import './Forms.css';
 import React, { Component } from "react";
-export default class Forms extends Component {
+export default class Form extends Component {
     constructor(props) {
-      super(props)
-      this.state = {
-         fname:"",
-         lname:"",
-         email:"",
-         username:"",
-         enterpassword:"",
-         confirmpassword:""
-      }
+        super(props)
+        this.state = {
+            fname: "",
+            lname: "",
+            email: "",
+            username: "",
+            enterpassword: "",
+            confirmpassword: ""
+        }
     }
-handleChange=(e)=>{
-    console.log(e.target.name);
-    var inputName = e.target.name
-    var newUser = {...this.state} // Creating a Copy of State and Storing in newUser
-    newUser[inputName] = e.target.value
-    this.setState(newUser)
-}
-addUser=()=>{
-  console.log(this.state)
-  this.clearForm()
-}
-clearForm=()=>{
-  this.setState({
-    fname:"",
-    lname:"",
-    email:"",
-    username:"",
-    enterpassword:"",
-    confirmpassword:""
- })
-}
-  render() {
-    return (
-      <div>
-        <form>
-          <label htmlFor="First Name">First Name</label>
-          <br/>
-          <input type="text" name="fname" onChange={(e)=>{this.handleChange(e)}} value={this.state.fname}/> <br />
-          <label htmlFor="Last Name">Last Name</label>
-          <br/>
-          <input type="text" name="lname" value={this.state.lname} onChange={(e)=>{this.handleChange(e)}} /> <br />
-          <label htmlFor="Email">Email</label>
-          <br/>
-          <input type="email" name="email" value={this.state.email} onChange={(e)=>{this.handleChange(e)}} /> <br />
-          <br/>
-          <label htmlFor="Username">Username</label>
-          <br/>
-          <input type="text" name="username" value={this.state.username} onChange={(e)=>{this.handleChange(e)}} /> <br />
-          <br/>
-          <label htmlFor="Enter Password">Enter Password</label>
-          <br/>
-          <input type="password" name="enterpassword" value={this.state.enterpassword} onChange={(e)=>{this.handleChange(e)}} /> <br />
-          <br/>
-          <label htmlFor="Confirm Password">Confirm Password</label>
-          <br/>
-          <input type="password" name="confirmpassword" value={this.state.confirmpassword} onChange={(e)=>{this.handleChange(e)}} /> <br />
-          <br/>
-          <a href="#">Back to Login</a>
-          <br/>
-          <button onClick={this.addUser} type="button">Register</button>
-        </form>
-      </div>
-    );
-  }
+    handleChange = (e) => {
+        console.log(e.target.name);
+        var inputName = e.target.name
+        var newUser = { ...this.state } // Creating a Copy of State and Storing in newUser
+        newUser[inputName] = e.target.value
+        this.setState(newUser)
+    }
+    addUser = () => {
+        console.log(this.state)
+        this.clearForm()
+    }
+    clearForm = () => {
+        this.setState({
+            fname: "",
+            lname: "",
+            email: "",
+            username: "",
+            enterpassword: "",
+            confirmpassword: ""
+        })
+    }
+    render() {
+        return (<div>
+                <div class="row">
+                    <div class="col-sm-4">
+                    </div>
+                    <div class="col-sm-4">
+                    </div>
+                    <div class="row"></div>
+                    <div class="col-sm-4">
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="signup_form">
+                            <form>
+                            <div class="labeltext" style={{"font-weight":"bold","text-align":"center","font-size":"30px"}}>Register</div>
+                                <div class="form-group">
+                                    <label htmlFor="First Name">First Name</label>
+                                    <input type="text" name="fname" onChange={(e) => { this.handleChange(e) }} value={this.state.fname} />
+                                </div>
+                                <div class="form-group">
+                                <label htmlFor="Last Name">Last Name</label>
+                                <input type="text" name="lname" value={this.state.lname} onChange={(e) => { this.handleChange(e) }} />
+                                </div>
+                                <div class="form-group">
+                                <label htmlFor="Email">Email</label>
+                                <input type="email" name="email" value={this.state.email} onChange={(e) => { this.handleChange(e) }} />
+                                </div>
+                                <div class="form-group">
+                                <label htmlFor="Username">Username</label>
+                                <input type="text" name="username" value={this.state.username} onChange={(e) => { this.handleChange(e) }} />
+                                </div>
+                                <div class="form-group">
+                                <label htmlFor="Enter Password">Enter Password</label>
+                                <input type="password" name="enterpassword" value={this.state.enterpassword} onChange={(e) => { this.handleChange(e) }} />
+                                </div>
+                                <div class="form-group">
+                                <label htmlFor="Confirm Password">Confirm Password</label>
+                                <input type="password" name="confirmpassword" value={this.state.confirmpassword} onChange={(e) => { this.handleChange(e) }} />
+                                </div>
+                                <a href="#">Back to Login</a>
+                                <br />
+                                <button onClick={this.addUser} type="button">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
