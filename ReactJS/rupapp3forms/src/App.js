bba1932f1095e0be3bb7fa67868d97e473398dee
reddyrups <121149033+reddyrups@users.noreply.github.com>
2022-12-27 12:42:51 +0530

    rup

--- ReactJS/rupapp3forms/src/App.js
+++ ReactJS/rupapp3forms/src/App.js
@@ -1,11 +1,12 @@
 import User from './Components/User';
 import './App.css';
 import Forms from './Components/Forms';
+import Registration from './Components/Registration';
 
 function App() {
   return (
     <div className="App">
-      <Forms/>
+      <Registration/>
     </div>
   );
 }
