030727d3c61d7d38e16e77893ad0de4589464813
reddyrups <121149033+reddyrups@users.noreply.github.com>
2022-12-27 11:16:15 +0530

    rup

---
+++ ReactJS/rupapp3forms/src/App.js
@@ -0,0 +1,13 @@
+import User from './Components/User';
+import './App.css';
+import Forms from './Components/Forms';
+
+function App() {
+  return (
+    <div className="App">
+      <Forms/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/rupapp3forms/src/Components/Forms.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+export default class Forms extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+         fname:"",
+         lname:"",
+         email:"",
+         username:"",
+         enterpassword:"",
+         confirmpassword:""
+      }
+    }
+handleChange=(e)=>{
+    console.log(e.target.name);
+    var inputName = e.target.name
+    var newUser = {...this.state} // Creating a Copy of State and Storing in newUser
+    newUser[inputName] = e.target.value
+    this.setState(newUser)
+}
+addUser=()=>{
+  console.log(this.state)
+  this.clearForm()
+}
+clearForm=()=>{
+  this.setState({
+    fname:"",
+    lname:"",
+    email:"",
+    username:"",
+    enterpassword:"",
+    confirmpassword:""
+ })
+}
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="First Name">First Name</label>
+          <br/>
+          <input type="text" name="fname" onChange={(e)=>{this.handleChange(e)}} value={this.state.fname}/> <br />
+          <label htmlFor="Last Name">Last Name</label>
+          <br/>
+          <input type="text" name="lname" value={this.state.lname} onChange={(e)=>{this.handleChange(e)}} /> <br />
+          <label htmlFor="Email">Email</label>
+          <br/>
+          <input type="email" name="email" value={this.state.email} onChange={(e)=>{this.handleChange(e)}} /> <br />
+          <br/>
+          <label htmlFor="Username">Username</label>
+          <br/>
+          <input type="text" name="username" value={this.state.username} onChange={(e)=>{this.handleChange(e)}} /> <br />
+          <br/>
+          <label htmlFor="Enter Password">Enter Password</label>
+          <br/>
+          <input type="password" name="enterpassword" value={this.state.enterpassword} onChange={(e)=>{this.handleChange(e)}} /> <br />
+          <br/>
+          <label htmlFor="Confirm Password">Confirm Password</label>
+          <br/>
+          <input type="password" name="confirmpassword" value={this.state.confirmpassword} onChange={(e)=>{this.handleChange(e)}} /> <br />
+          <br/>
+          <a href="#">Back to Login</a>
+          <br/>
+          <button onClick={this.addUser} type="button">Register</button>
+        </form>
+      </div>
+    );
+  }
+}
