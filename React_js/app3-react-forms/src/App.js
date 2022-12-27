ae2b96deedc420b04ce8755c50541def9fdb9c7d
ChVManjunadhaReddy <chilakala.reddy@incedoinc.com>
2022-12-27 12:37:53 +0530

    In-Form

--- React_js/app3-react-forms/src/App.js
+++ React_js/app3-react-forms/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import User from './Components/User';
 import Form from './Components/Form';
+import Registration from './Components/Registration';
 
 function App() {
   return (
     <div>
-    <Form/>
+    <Registration/>
     </div>
   );
 }
