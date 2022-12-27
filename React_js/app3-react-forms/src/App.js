cd9c988caa1298ca07fbd539b8421ae8ca637b24
ChVManjunadhaReddy <chilakala.reddy@incedoinc.com>
2022-12-27 11:16:50 +0530

    SampleForm

--- React_js/app3-react-forms/src/App.js
+++ React_js/app3-react-forms/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './Components/User';
+import Form from './Components/Form';
 
 function App() {
   return (
     <div>
-    <User/>
+    <Form/>
     </div>
   );
 }
