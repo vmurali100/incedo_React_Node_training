a5fd6559ed7b72a428c48ad01845a584bfe6d30a
nishevidhaam <nishevidhaa@gmail.com>
2022-12-27 11:18:37 +0530

    register form

--- React/app01/src/Person.jsx
+++ React/app01/src/Person.jsx
@@ -3,20 +3,52 @@ import { Component } from "react";
 class Person extends Component{
     constructor(){
         super()
-        this.state ={
-            fname:"Nishevidhaa",
-            lname:"Muralibarathy",
-            Native:"Salem"
-            
-        };
-    }
+        this.state = {
+            isShowMesagge: false,
+            welcomemessage: "hi all welcome to training",
+            fname: "Nishevidhaa",
+            personDetails: {
+              email: "nishevidhaa@gmail.com",
+              city: "Salem",
+              state:"TN",
+              langulage:"Tamil"
+            },
+            subjects: ["HTML", "CSS", "JS", "ReactJS","NodeJS","MySql","Express"],
+          };
+          this.showMessage = this.showMessage.bind(this);
+        }
+        showMessage() {
+        //   this.state.isShowMesagge = true
+          this.setState({ isShowMesagge: true });
+        }
+      
+        // showMessage = ()=>{
+        //     this.setState({isShowMesagge:true})
+        // }
+        
     render(){
+        const { WelcomeMessage } = this.state;
         
         return (
             <div>
-            <h3>{this.state.fname}</h3>
+                <button onClick={this.showMessage}>Show Message </button>
+                {this.state.isShowMesagge && <h2>{WelcomeMessage}</h2>}
+            {/* <h3>{this.state.fname}</h3>
             <h3>{this.state.lname}</h3>
-            <h3>{this.state.Native}</h3>
+            <h3>{this.state.Native}</h3> */}
+            
+            {/* <ul>
+            {Object.keys(this.state.personDetails).map((val)=>{
+                return <li>{this.state.personDetails[val]}</li>
+            })}
+        </ul> */}
+            {/* <ul>
+                <ul>
+            {this.state.subjects.map((val,i)=>{
+                return <li key={i}>{val}</li>
+            })}
+        </ul>
+            </ul> */}
             </div>
         )
        
---
+++ React/forms/src/User1.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+export default class User1 extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         fname:"",
+         lname:"",
+         email:"",
+         username:"",
+         password:"",
+         confirmpassword:""
+      }
+    }
+    
+handleChange=(e)=>{
+    console.log(e.target.name);
+    var inputName = e.target.name
+    var newUser = {...this.state} // Creating a Copy of State and Storing in newUser
+    newUser[inputName] = e.target.value
+    this.setState(newUser)
+    
+  
+}
+
+addUser=()=>{
+  console.log(this.state)
+  const { password, confirmpassword } = this.state;
+      // perform all neccassary validations
+      if (password !== confirmpassword) {
+          alert("Passwords don't match");
+      } else {
+          // make API call
+      }
+  this.clearForm()
+  
+}
+
+clearForm=()=>{
+  this.setState({
+    fname:"",
+    lname:"",
+    email:"",
+    username:"",
+    password:"",
+    confirmpassword:""
+ })
+}
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="First Name">First Name : </label>
+          <input type="text" name="fname" onChange={(e)=>{this.handleChange(e)}} value={this.state.fname}/> <br />
+          <label htmlFor="Last Name">Last Name : </label>
+          <input type="text" name="lname" value={this.state.lname} onChange={(e)=>{this.handleChange(e)}} /> <br />
+          <label htmlFor="email">Email Id : </label>
+          <input type="text" name="email" onChange={(e)=>{this.handleChange(e)}} value={this.state.email}/> <br />
+          <label htmlFor="username">User Name : </label>
+          <input type="text" name="username" onChange={(e)=>{this.handleChange(e)}} value={this.state.username}/> <br />
+          <label htmlFor="password">Password : </label>
+          <input type="passowrd" name="password" onChange={(e)=>{this.handleChange(e)}} value={this.state.password}/> <br />
+          <label htmlFor="confirmpassword">Confirm Password : </label>
+          <input type="password" name="confirmpassword" onChange={(e)=>{this.handleChange(e)}} value={this.state.confirmpassword}/> <br />
+          <button onClick={this.addUser} type="button">Register User</button>
+        </form>
+      </div>
+    );
+  }
+}
