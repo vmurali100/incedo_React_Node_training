14629f635fbd4cf77f32c5f68e34d1c4ff164a61
ChVManjunadhaReddy <chilakala.reddy@incedoinc.com>
2022-12-28 12:32:21 +0530

    Added axios

---
+++ React_js/app4-react-crud-axios/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from './Components/Registration';
+
+function App() {
+  return (
+    <div>
+     <Registration/> 
+    </div>
+  );
+}
+
+export default App;
