fe94caccccf5f4f6a3c8e6c656864a66f88880a2
vmurali100 <vmurali100@users.noreply.github.com>
2022-12-28 10:02:12 +0530

    Creating Table

--- ReactJS/app3-react-forms/src/Components/Registration.jsx
+++ ReactJS/app3-react-forms/src/Components/Registration.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import UserForm from "./UserForm";
 import UserTable from "./UserTable";
 
 const Registration = () => {
+  const [user, setUser] = useState({ fname: "", lname: "", email: "" });
+  const [users,setUsers] = useState([])
+
+  const handleChange = (e) => {
+    let newUser = { ...user };
+    newUser[e.target.name] = e.target.value;
+    setUser(newUser);
+  };
+
+  const handleSubmit = () => {
+    console.log(user);
+    let newUsers = [...users];
+    newUsers.push(user);
+    setUsers(newUsers)
+    clearForm();
+  };
+
+  const clearForm = () => {
+    setUser({ fname: "", lname: "", email: "" });
+  };
+
   return (
     <div>
-      {/* <h2>Hello from Registration</h2> */}
       <div className="row">
         <div className="col-4">
-            <UserForm/>
+          <UserForm
+            user={user}
+            setUser={setUser}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            clearForm={clearForm}
+          />
         </div>
         <div className="col-8">
-            <UserTable/>
+          <UserTable users={users} />
         </div>
-       
       </div>
       {/* <form>
         <div className="mb-3">
--- ReactJS/app3-react-forms/src/Components/UserTable.jsx
+++ ReactJS/app3-react-forms/src/Components/UserTable.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-const UserTable = () => {
+const UserTable = ({users}) => {
+  console.log(users)
   return (
     <div>
       <table className="table">
         <thead>
           <tr>
-            <th>#</th>
             <th>First</th>
             <th>Last</th>
             <th>Email</th>
           </tr>
         </thead>
         <tbody>
-         
+          {users.map((user,i)=> <tr key={i}>
+            <td>{user.fname}</td>
+            <td>{user.lname}</td>
+            <td>{user.email}</td>
+          </tr> )}
         </tbody>
       </table>
     </div>
