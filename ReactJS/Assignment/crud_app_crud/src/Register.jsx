6308b92fa113d41c93b0503d1937f6117ed00bcc
chrisR47 <chris.manivannan@incedoinc.com>
2022-12-28 06:42:15 +0000

    Day 4 - Axios With CRUD

--- ReactJS/Assignment/crud_app_crud/src/Register.jsx
+++ ReactJS/Assignment/crud_app_crud/src/Register.jsx
@@ -1,17 +1,23 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Fragment } from "react";
 import { startState } from "./components/Forms";
 import FunctionalForms from "./components/FunctionalComponentsForms";
 import Table from "./UserTable";
-
+import axios from 'axios';
 function Register()
 {
  
     const [users,setUsers]=useState([]);
     const [user, setUser] = useState({ ...startState });
     const [editIndex,setEditIndex]=useState(null);
-   
+    useEffect(()=>
+    {
+      getAllUsers()
+    },[]);
+    const getAllUsers=()=>
+    {
+      axios.get("http://localhost:3000/user").then((res)=>setUsers(res.data)).catch(err=>console.log('Error',err));
+    }
     const handleChange = (e) => {
       const name = e.target.name;
       const newState = { ...user };
@@ -19,10 +25,9 @@ function Register()
       setUser(newState);
     };
     const handleClick = function () {
-      console.log("User :", user);
-      const newUsers=[...users];
-      newUsers.push(user);
-      setUsers(newUsers)
+     axios.post("http://localhost:3000/user",user).then((res)=>{console.log(res);getAllUsers()}).catch(()=>{
+      console.log("testings")
+     })
       setUser(startState);
     };
     const handleEdit=(userEdit,i)=>
@@ -32,15 +37,17 @@ function Register()
     }
     const handleDelete=(userdelete)=>
     {
-        setUsers(users.filter((user)=>user.fname!==userdelete.fname));
+       axios.delete("http://localhost:3000/user/"+userdelete.id).then((res)=>{console.log(res);getAllUsers();}).catch((err)=>console.log("error",err))
     }
     const updateUser=()=>
     {
-      const newUsers=[...users];
-      newUsers[editIndex]=user;
-      setUsers(newUsers)
-      setEditIndex(null);
-      setUser({...startState})
+      axios.put("http://localhost:3000/user/"+user.id,user).then((res)=>
+     {
+       console.log(res.data);
+       getAllUsers();
+       setUser(startState);
+       setEditIndex(null);
+     }).catch((err)=>console.log("error",err))
     }
     return <Fragment>
         <FunctionalForms user={user} handleChange={handleChange} handleClick={handleClick} editIndex={editIndex} updateUser={updateUser}/>
---
+++ ReactJS/Assignment/crud_app_crud/src/UserTable.jsx
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+
+function Table({users,handleDelete,handleEdit})
+{
+    console.log(users)
+    return <Fragment>
+        {!!users &&
+       <table>
+        <thead>
+            <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Phone</th> 
+          <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+            {users.map((user,i)=>
+            {
+                return <tr key={i}>
+                    <td>{user.fname}</td>
+                    <td>{user.lname}</td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                    <td>{user.username}</td>
+                    <td><button onClick={()=>handleEdit(user,i)}>Edit</button></td>
+                    <td><button onClick={()=>handleDelete(user)}>Delete</button></td>
+                </tr>
+            })}
+
+        </tbody>
+       </table>
+}
+    </Fragment>
+}
+export default Table;
