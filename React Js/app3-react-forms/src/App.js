53313f541db78f1479dedf3ca460d5daf963dea0
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2022-12-28 10:17:53 +0530

    ganesh

--- React Js/app3-react-forms/src/App.js
+++ React Js/app3-react-forms/src/App.js
@@ -1,5 +1,4 @@
 import User from './Components/User';
-import './App.css';
 import Form from './Components/Form';
 import Registration from './Components/Registration';
 import Userfun from './Components/UserFun';
@@ -7,7 +6,7 @@ import Userfun from './Components/UserFun';
 function App() {
   return (
     <div className="App">
-      <Userfun/>
+      <Registration/>
     </div>
   );
 }
