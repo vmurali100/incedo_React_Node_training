71c5ffad8fe8ce12a0c2ca88915988e97ca743cb
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2022-12-28 11:10:53 +0530

    ganesh

--- React Js/app3-react-forms/src/Components/Registration.jsx
+++ React Js/app3-react-forms/src/Components/Registration.jsx
@@ -4,7 +4,8 @@ import UserTable from "./UserTable";
 
 const Registration = () => {
   const [user, setUser] = useState({ fname: "", lname: "", email: "" });
-  const [users,setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [editIndex,setEditIndex] = useState(null)
 
   const handleChange = (e) => {
     let newUser = { ...user };
@@ -13,16 +14,33 @@ const Registration = () => {
   };
 
   const handleSubmit = () => {
-    console.log(user);
     let newUsers = [...users];
     newUsers.push(user);
-    setUsers(newUsers)
+    setUsers(newUsers);
     clearForm();
   };
 
   const clearForm = () => {
     setUser({ fname: "", lname: "", email: "" });
   };
+
+  const deleteuser = (user) => {
+    var newUsers = users.filter((usr) => usr.fname !== user.fname);
+    setUsers(newUsers);
+  };
+
+  const editUser = (user,i)=>{
+    setEditIndex(i)
+    setUser(user)
+  }
+
+  const updateUser = ()=>{
+    let newUsers = [...users]
+    newUsers[editIndex] = user;
+    setUsers(newUsers)
+    setEditIndex(null)
+    clearForm()
+  }
 
   return (
     <div>
@@ -34,10 +52,12 @@ const Registration = () => {
             handleChange={handleChange}
             handleSubmit={handleSubmit}
             clearForm={clearForm}
+            updateUser={updateUser}
+            editIndex={editIndex}
           />
         </div>
         <div className="col-8">
-          <UserTable users={users} />
+          <UserTable users={users} deleteuser={deleteuser} editUser={editUser} />
         </div>
       </div>
       {/* <form>
