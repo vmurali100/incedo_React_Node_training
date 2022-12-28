ee0ab9ca4ecaa77d3946281b6143e4c74aac778b
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2022-12-28 13:39:05 +0530

    ganesh

---
+++ React Js/app6-react-crud-spa/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ReadUsers from "./Components/ReadUsers";
+import CreateUser from "./Components/CreateUser";
+import UpdateUsers from "./Components/UpdateUsers";
+import DeleteUser from "./Components/DeleteUser";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <NavBar/>
+        <Routes>
+          <Route path="/" element={<ReadUsers />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/update" element={<UpdateUsers />} />
+          <Route path="/delete" element={<DeleteUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
