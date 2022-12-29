dcc40f1536e90d2e3ad91a92d75b9cf4bb1e4bd8
2910naveen <segu.naveen@incedoinc.com>
2022-12-29 15:05:27 +0530

    "Day5 commits"

--- app6-react-crud-spa/src/Components/CreateUser.js
+++ app6-react-crud-spa/src/Components/CreateUser.js
@@ -1,11 +1,40 @@
 import React,{Component} from "react";
+import UserForm from "./UserForm";
+import axios from 'axios';
 
 class CreateUser extends Component
 {
+    constructor(props) 
+    {
+        super(props);
+        this.state={
+            user:{fname:"",
+                  lname:"",
+                  email:""}
+        }
+    }
+    handleChange=(e)=>
+    {
+       var newUser = {...this.state.user};
+       newUser[e.target.name] = e.target.value;
+       this.setState({user:newUser});
+    }
+    handleSubmit=()=>{
+        axios.post("http://localhost:3000/users"+this.state.user).then(()=>{
+            clearForm();
+        });
+    }
+    clearForm = ()=>
+    {
+        this.setState({user:{fname:"",
+        lname:"",
+        email:""}});
+    }
     render()
     {
         return <>
                <h1>CreateUser</h1>
+               <UserForm user={this.state.user} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
                </>
     };
 };
