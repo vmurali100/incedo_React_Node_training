00018ab1f7e6f20c48816ea5257e856e49a3ce09
Amarendranjf <114225658+Amarendranjf@users.noreply.github.com>
2022-12-29 11:27:19 +0530

    amar

--- Reactjs/app6-react-crud-spa/src/App.js
+++ Reactjs/app6-react-crud-spa/src/App.js
@@ -1,24 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ReadUsers from "./Components/ReadUsers";
-import CreateUser from "./Components/CreateUser";
-import UpdateUsers from "./Components/UpdateUsers";
-import DeleteUser from "./Components/DeleteUser";
-import NavBar from "./Components/NavBar";
+import logo from './logo.svg';
+import './App.css';
+import SampleTask from './components/SampleTask';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <NavBar/>
-        <Routes>
-          <Route path="/" element={<ReadUsers />} />
-          <Route path="/create" element={<CreateUser />} />
-          <Route path="/update" element={<UpdateUsers />} />
-          <Route path="/delete" element={<DeleteUser />} />
-        </Routes>
-      </BrowserRouter>
+      <SampleTask/>
     </div>
   );
 }
