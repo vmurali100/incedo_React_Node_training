b49b7e5eaeb4f7c0cedaf064b8cd82cbdd881670
Suchi465 <107851318+Suchi465@users.noreply.github.com>
2022-12-29 15:07:33 +0530

    DAY-4

--- ReactJS/Assignment/form3/src/App.js
+++ ReactJS/Assignment/form3/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Form1 from "./components/Form1Class";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form1 />
     </div>
   );
 }
