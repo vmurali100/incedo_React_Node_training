aec013cbdcfeaf567fd662e2c55476f5b31c2a41
uvan1603 <105769785+uvan1603@users.noreply.github.com>
2022-12-29 09:37:58 +0530

    Add files via upload

---
+++ app3-react-forms/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Registration from "./Components/Registration";
+
+function App() {
+  return (
+    <div className="App container">
+      {/* <User /> */}
+      <Registration/>
+    </div>
+  );
+}
+
+export default App;
