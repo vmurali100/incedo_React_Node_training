02d1695e68b56706c09f31fdfc1fa00efae15865
uvan1603 <105769785+uvan1603@users.noreply.github.com>
2022-12-29 09:39:01 +0530

    Add files via upload

---
+++ app4-crud-axios/src/Components/UserForm.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState } from "react";
+
+const UserForm = ({ user, handleChange, handleSubmit, updateUser, editIndex }) => {
+
+  return (
+    <div>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="fname" className="form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            value={user.fname}
+            name="fname"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="lname" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            value={user.lname}
+            name="lname"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            value={user.email}
+            name="email"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+
+        {editIndex !== null ? (
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={updateUser}
+          >
+            Update User
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
