4c46604f664e01f3e11178f20225051f2eab1b10
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2022-12-30 13:34:56 +0530

    ganesh

---
+++ React Js/app10-hoc-project/src/Components/ClickCounter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import HOCComp from "./HOCComp";
+
+class ClickCounter extends Component {
+ 
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.handleIncrement}>Click</button>
+        <h2>You Clicked {this.props.count} Times !!</h2>
+      </div>
+    );
+  }
+}
+
+export default HOCComp(ClickCounter)
--- React Js/app9-react-auth-project/src/App.js
+++ React Js/app9-react-auth-project/src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Features from "./Components/Features";
+import { AuthProvider } from "./Components/auth";
+import RequiredAuth from "./Components/RequiredAuth";
 
 function App() {
   return (
     <div className="App">
-      
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/features"
+              element={
+                <RequiredAuth>
+                  <Features />
+                </RequiredAuth>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
