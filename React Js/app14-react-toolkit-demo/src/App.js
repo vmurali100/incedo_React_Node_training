2b6d0b245477190159469eb47624d8fe8dc3c370
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2023-01-04 13:07:47 +0530

    ganesh

--- React Js/app14-react-toolkit-demo/src/App.js
+++ React Js/app14-react-toolkit-demo/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Users from './Components/Users';
+import Register from './Components/Register';
 
 function App() {
   return (
     <div className="App">
       <Users/>
+      <Register/>
     </div>
   );
 }
--- React Js/app14-react-toolkit-demo/src/Components/Users.jsx
+++ React Js/app14-react-toolkit-demo/src/Components/Users.jsx
@@ -1,27 +1,37 @@
 import React from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addUserAction, deleteUser } from "../store/usersSlice";
+import {
+  addUserAsyncAction,
+  deleteUser,
+  getUsersAsyncAction,
+} from "../store/usersSlice";
 
 const Users = () => {
   const usersDetails = useSelector((state) => state.users);
-  console.log(usersDetails)
-  const dispatch = useDispatch()
-  console.log(usersDetails);
-  const handleDelete=(i)=>{
-    dispatch(deleteUser(i))
-  }
+  const dispatch = useDispatch();
+  const handleDelete = (i) => {
+    dispatch(deleteUser(i));
+  };
 
-  const addUser=()=>{
-    dispatch(addUserAction({
-        "id": 379,
-        "email": "murali@magna.org",
-        "username": "murali",
-        "password": "1234"
-      }))
-  }
+  useEffect(() => {
+    dispatch(getUsersAsyncAction());
+  }, []);
+  const addUser = () => {
+    dispatch(
+      addUserAsyncAction({
+        id: null,
+        email: "murali@magna.org",
+        username: "murali",
+        password: "1234",
+      })
+    );
+  };
   return (
     <div className="container">
-        <button onClick={addUser}>Add User</button>
+      <button onClick={addUser} className="btn btn-primary">
+        Add User
+      </button>
       <table className="table">
         <thead>
           <tr>
@@ -34,18 +44,27 @@ const Users = () => {
           </tr>
         </thead>
         <tbody>
-            {usersDetails.users.map((user,i)=> <tr key={i}>
-                <td>{user.id}</td>
-                <td>{user.email}</td>
-                <td>{user.username}</td>
-                <td>{user.password}</td>
-                <td>
-                    <button className="btn btn-warning">Edit User</button>
-                </td>
-                <td>
-                    <button className="btn btn-danger" onClick={()=>{handleDelete(i)}}>Delete User</button>
-                </td>
-            </tr> )}
+          {usersDetails.users.map((user, i) => (
+            <tr key={i}>
+              <td>{user.id}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.password}</td>
+              <td>
+                <button className="btn btn-warning">Edit User</button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => {
+                    handleDelete(i);
+                  }}
+                >
+                  Delete User
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
