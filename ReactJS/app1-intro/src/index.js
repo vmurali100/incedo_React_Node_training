0f60e651fd4c9830967514ad7c66363d611397f9
bhargavbhavirisetty <bhargavbhavirisetty2000@gmail.com>
2023-01-04 22:27:02 +0530

    commit

--- ReactJS/app1-intro/src/index.js
+++ ReactJS/app1-intro/src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Registration from './Form2/Registration';
-import EmpRegistration from './Form3/EmpRegistration';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./book/reducers"
 import reportWebVitals from './reportWebVitals';
+
+
+
+
+const store = createStore(reducers);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(store)
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>
+);
+
 {/*
 import Component1 from './Components/Component1';
 import { Person } from './Classes/Person';
@@ -13,14 +29,21 @@ import Registration from './Forms/Registration';
 import Form1 from './Assignment_Day3/Form1/q1';
 import UserForm from './Assignment_Day3/Form1/q1.function';
 import User1 from './Assignment_Day3/Form2/q2';
+import Question17 from './Mid_Assignment/Question17';
+import Question19 from './Mid_Assignment/Question19';
+import SampleTask from './Mid_Assignment/Question20';
+import Counter from './Mid_Assignment/Counter';
+import Registration from './Form2/Registration';
+import EmpRegistration from './Form3/EmpRegistration';
 */}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+{/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Question17/>
   </React.StrictMode>
-);
+);*/}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
