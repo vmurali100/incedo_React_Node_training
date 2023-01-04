07dc1a940cfd34211f46ce23ee8777affd11606c
Raghul1704 <raghul.s@incedoinc.com>
2023-01-04 11:05:58 +0530

    commit in raghul branch

---
+++ ReactJS/app12-react-redux-demo/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Users from './Components/Users';
+import Products from './Components/Products';
+
+function App() {
+  return (
+    <div className="App">
+      <Users/>
+      <hr/>
+      {/* <Products/> */}
+    </div>
+  );
+}
+
+export default App;
