1b49dce71a83faad60a4549fa49317479697c594
vmurali100 <vmurali100@users.noreply.github.com>
2023-01-04 12:38:19 +0530

    Adding Redux toolkit

--- ReactJS/app14-react-toolkit-demo/src/store/usersSlice.js
+++ ReactJS/app14-react-toolkit-demo/src/store/usersSlice.js
@@ -1,6 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { defaultState } from "./defaultState";
+import axios from "axios";
 
+export const getUsersAsyncAction = createAsyncThunk(
+  "users/getUsersAsyncAction",
+  () => handleGetAllUsers()
+);
+
+export const addUserAsyncAction = createAsyncThunk(
+  "users/addUserAsyncAction",
+  (user) =>
+    axios.post("http://localhost:3201/users/", user).then(async (res) => {
+      const finalPayload = await handleGetAllUsers();
+      return finalPayload;
+    })
+);
+
+const handleGetAllUsers = () =>
+  axios.get("http://localhost:3201/users").then((res) => res.data);
+  
 export const usersSlice = createSlice({
   name: "users",
   initialState: defaultState,
@@ -11,6 +29,20 @@ export const usersSlice = createSlice({
     deleteUser: (state, action) => {
       state.users.splice(action, 1);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getUsersAsyncAction.fulfilled, (state, action) => {
+      state.users = action.payload;
+    });
+    builder.addCase(addUserAsyncAction.fulfilled, (state, action) => {
+      state.users = action.payload;
+    });
+    // builder.addCase(getUsersAsyncAction.pending, (state, action) => {
+    //   console.log("getUsersAsyncAction Pending !!");
+    // });
+    // builder.addCase(getUsersAsyncAction.rejected, (state, action) => {
+    //   console.log("getUsersAsyncAction rejected !!");
+    // });
   },
 });
 
