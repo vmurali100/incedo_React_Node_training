b4c8bc96ff071fa1ec2307f027fe27eac360146a
SriganeshIncedo <mullapudi.sriganesh@incedoinc.com>
2023-01-04 11:54:53 +0530

    ganesh

--- React Js/app12-react-redux-demo/src/reducers/UsersReducer.js
+++ React Js/app12-react-redux-demo/src/reducers/UsersReducer.js
@@ -2,14 +2,12 @@ import { ADD_USER } from "../actions/actionTypes";
 import { defaultState } from "./defaultState";
 
 export const usersReducer = (state = defaultState, action) => {
-    console.log("usersReducer called")
+  console.log("usersReducer called");
   switch (action.type) {
     case ADD_USER:
       let newUsers = [...state.users];
       newUsers.push(action.payload);
-
       return { ...state, users: newUsers };
-
     default:
       return state;
   }
