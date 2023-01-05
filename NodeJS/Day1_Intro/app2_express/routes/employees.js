152a32cb619af98bc7015d76695a0ca0a046f768
vmurali100 <vmurali100@users.noreply.github.com>
2023-01-05 12:59:17 +0530

    read a data from File

--- NodeJS/Day1_Intro/app2_express/routes/employees.js
+++ NodeJS/Day1_Intro/app2_express/routes/employees.js
@@ -1,39 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/',(req,res)=>{
+
     res.json({message:"Welcome to Employees !!!"})
 })
 router.get('/allEmployees',(req,res)=>{
-    res.send({allEmployees})
+    // res.send({allEmployees})
+    fs.readFile('./data/employees_data.json',(err,data)=>{
+        var buf = Buffer.from(data);
+        res.send({allEmployees:JSON.parse(buf.toString())})
+    })
 })
 
 module.exports = router;
 
-const allEmployees = [
-    {
-        "id": 112,
-        "email": "NAbrahams@sed.gov",
-        "username": "CPtacek"
-    },
-    {
-        "id": 113,
-        "email": "GMawyer@pretium.gov",
-        "username": "MLazier"
-    },
-    {
-        "id": 114,
-        "email": "MTravis@ipsum.net",
-        "username": "LClore"
-    },
-    {
-        "id": 115,
-        "email": "NRooprai@rutrum.gov",
-        "username": "LBlankenship"
-    },
-    {
-        "id": 116,
-        "email": "DStudenka@sollicitudin.org",
-        "username": "APopsikle"
-    }
-]
