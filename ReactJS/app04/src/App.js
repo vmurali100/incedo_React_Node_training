af3addeaff537a488f3c37a077d102c91b0a3e28
Nanditha115 <nandithap1107@gmail.com>
2023-01-05 09:54:27 +0530

    app04

---
+++ ReactJS/app04/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Registration from './Components/Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration/>
+    </div>
+  );
+}
+
+export default App;
