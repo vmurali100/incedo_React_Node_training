39c1f491219099ea465da18dfa48b06e46e389eb
Tejusree <teja.mb@incedoinc.com>
2023-01-05 13:20:07 +0530

    commit

--- NodeJS/Day_1_Intro/app2_express/app.js
+++ NodeJS/Day_1_Intro/app2_express/app.js
@@ -2,18 +2,16 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var employeesRouter = require('./routes/employees')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/employees',employeesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ NodeJS/Day_1_Intro/app2_express/routes/employees.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+var fs = require("fs");
+
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to Employees !!!" });
+});
+
+router.get("/allEmployees", (req, res) => {
+  fs.readFile("./data/employees_data.json", (err, data) => {
+    var buf = Buffer.from(data);
+    res.send({ allEmployees: JSON.parse(buf.toString()) });
+  });
+});
+
+module.exports = router;
