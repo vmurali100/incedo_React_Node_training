6495e2d7c55e0fbc60b6d5b181aa3919b3c363b5
Sathyanarayanan P S <sathyanarayanan.ps@incedoinc.com>
2023-01-05 09:53:39 +0530

    done

---
+++ redux_demo/src/Components/Users.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector,} from "react-redux";
+import {
+  addUserAsyncAction,
+  deleteUser,
+  getUsersAsyncAction,
+} from "../store/usersSlice";
+
+const Users = () => {
+  const usersDetails = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  const handleDelete = (i) => {
+    dispatch(deleteUser(i));
+  };
+
+  useEffect(() => {
+    dispatch(getUsersAsyncAction());
+  }, []);
+  const addUser = () => {
+    dispatch(
+      addUserAsyncAction({
+        id: null,
+        email: null,
+        username: null,
+        password: null,
+      })
+    );
+  };
+  return (
+    <div className="container">
+      <button onClick={addUser} className="btn btn-primary">
+        Add User
+      </button>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Email</th>
+            <th>User Name</th>
+            <th>Password</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersDetails.users.map((user, i) => (
+            <tr key={i}>
+              <td>{user.id}</td>
+              <td>{user.email}</td>
+              <td>{user.username}</td>
+              <td>{user.password}</td>
+              <td>
+                <button className="btn btn-warning">Edit User</button>
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => {
+                    handleDelete(i);
+                  }}
+                >
+                  Delete User
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Users;
