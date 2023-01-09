968c84ac0b94b1e0850d03d5ad99fa89a2051fd0
Raghul1704 <raghul.s@incedoinc.com>
2023-01-09 13:02:43 +0530

    node commit 2

--- NodeJS/Day1_Intro/app2_express/routes/employees.js
+++ NodeJS/Day1_Intro/app2_express/routes/employees.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -39,6 +40,8 @@ const allEmployees = [
     }
 ]
 =======
+=======
+>>>>>>> 0a279386ddac8a6cf7c59a877aa9ad281e64522f
 var express = require("express");
 var router = express.Router();
 var fs = require("fs");
@@ -53,5 +56,53 @@ router.get("/allEmployees", (req, res) => {
   });
 });
 
+<<<<<<< HEAD
 module.exports = router;
 >>>>>>> e7e10d628cae7290683bb487fd664591b397d070
+=======
+router.post("/createEmployee", async (req, res) => {
+  const employee = req.body;
+  let currentEmployees = await getLatestEmployeesData();
+  currentEmployees.push(employee);
+  let allEmployees = await addEmployees(currentEmployees);
+  res.json({ allEmployees });
+});
+
+//   currentEmployees.
+//   fs.readFile("./data/employees_data.json", (err, data) => {
+//     var buf = Buffer.from(data);
+//     var allEmployeesData = JSON.parse(buf.toString());
+//     allEmployeesData.push(employee);
+//   fs.writeFile(
+//     "./data/employees_data.json",
+//     JSON.stringify(allEmployeesData),
+//     async (err, data) => {
+//       var allEmployees = await getLatestEmployeesData();
+//       res.json({ allEmployees });
+//     }
+//   );
+//   });
+
+function getLatestEmployeesData() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./data/employees_data.json", (err, data) => {
+      var buf = Buffer.from(data);
+      var allEmployeesData = JSON.parse(buf.toString());
+      resolve(allEmployeesData);
+    });
+  });
+}
+function addEmployees(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(
+      "./data/employees_data.json",
+      JSON.stringify(data),
+      async (err, data) => {
+        var allEmployees = await getLatestEmployeesData();
+        resolve(allEmployees);
+      }
+    );
+  });
+}
+module.exports = router;
+>>>>>>> 0a279386ddac8a6cf7c59a877aa9ad281e64522f
