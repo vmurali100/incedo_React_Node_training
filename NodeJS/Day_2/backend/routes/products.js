3fb024989f01f936990805d79d691f72161c375e
Tejusree <teja.mb@incedoinc.com>
2023-01-09 16:40:48 +0530

    commit

--- NodeJS/Day_2/backend/routes/products.js
+++ NodeJS/Day_2/backend/routes/products.js
@@ -1,32 +1,53 @@
-var express = require('express')
-var fs = require('fs');
-const { router } = require('../app');
-var route = express.Router();
+var express = require("express");
+var router = express.Router();
+var jwt = require("jsonwebtoken");
+var fs = require("fs");
+const e = require("express");
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.json({ title: "Welcme to Products !!" });
+});
 
-router.post("/createProduct", (asyncreq,res)=>{
+router.post("/addProduct", checkUser, async (req, res) => {
   var product = req.body;
-
-  fs.readFileSync('./data/products.json',(err,data)=>{
-    var products = JSON.parse(Buffer.from(data).toString());
-    product.push(product)
-    addProduct(products)
-    let allProducts=await getLatestProducts();
-  })
-})
-
-function addProduct(products){
-    fs.writeFile("./data/products.json", JSON.stringify(products),()=>{
-        console.log("Product added")
-    })
+  var allProducts = await getAllProducts();
+  var check = checkProduct(allProducts, product);
+  if(check){
+    res.send({msg:"The Product is Already exist . Please add a New product "})
+  }else{
+    allProducts.push(product);
+    var allLatestProducts = await addProduct(allProducts);
+    res.send({ msg: "Product Added Successfully ", products: allLatestProducts });
+  }
+ 
+});
+function checkUser(req, res, next) {
+  console.log(req.headers.authorization);
+  const tokenDetails = req.headers.authorization.split(" ")[1];
+  try {
+    const verifyDetails = jwt.verify(tokenDetails, "some secrete word");
+    next();
+  } catch (error) {
+    res.send({ msg: "User is not Authorized", error });
+  }
 }
 
-function getLatestProducts(){
-    return new Promise((resolve,reject)=>{
-        fs.readFile('./data/products.json', (err,data)=>{
-        var buf = Buffer.from(data);
-            var allProducts = JSON.parse(buf.from(data).toString());
-        resolve(allProducts)
-        })       
-    
-    })
-}
+const getAllProducts = () =>
+  new Promise((resolve, reject) => {
+    fs.readFile("./data/products.json", (err, data) => {
+      var productsDetails = JSON.parse(Buffer.from(data).toString());
+      resolve(productsDetails);
+    });
+  });
+const addProduct = (products) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile("./data/products.json", JSON.stringify(products), async () => {
+      var latestProducts = await getAllProducts();
+      resolve(latestProducts);
+    });
+  });
+
+const checkProduct = (products, product) => {
+  return !!products.find((myProduct) => myProduct.productName === product.productName);
+};
+module.exports = router;
