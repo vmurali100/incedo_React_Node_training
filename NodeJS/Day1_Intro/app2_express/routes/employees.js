0a279386ddac8a6cf7c59a877aa9ad281e64522f
vmurali100 <vmurali100@users.noreply.github.com>
2023-01-09 11:00:33 +0530

    NODE JS CRUD With Authentication

--- NodeJS/Day1_Intro/app2_express/routes/employees.js
+++ NodeJS/Day1_Intro/app2_express/routes/employees.js
@@ -12,4 +12,48 @@ router.get("/allEmployees", (req, res) => {
   });
 });
 
+router.post("/createEmployee", async (req, res) => {
+  const employee = req.body;
+  let currentEmployees = await getLatestEmployeesData();
+  currentEmployees.push(employee);
+  let allEmployees = await addEmployees(currentEmployees);
+  res.json({ allEmployees });
+});
+
+//   currentEmployees.
+//   fs.readFile("./data/employees_data.json", (err, data) => {
+//     var buf = Buffer.from(data);
+//     var allEmployeesData = JSON.parse(buf.toString());
+//     allEmployeesData.push(employee);
+//   fs.writeFile(
+//     "./data/employees_data.json",
+//     JSON.stringify(allEmployeesData),
+//     async (err, data) => {
+//       var allEmployees = await getLatestEmployeesData();
+//       res.json({ allEmployees });
+//     }
+//   );
+//   });
+
+function getLatestEmployeesData() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./data/employees_data.json", (err, data) => {
+      var buf = Buffer.from(data);
+      var allEmployeesData = JSON.parse(buf.toString());
+      resolve(allEmployeesData);
+    });
+  });
+}
+function addEmployees(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(
+      "./data/employees_data.json",
+      JSON.stringify(data),
+      async (err, data) => {
+        var allEmployees = await getLatestEmployeesData();
+        resolve(allEmployees);
+      }
+    );
+  });
+}
 module.exports = router;
