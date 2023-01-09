677b1f67161a2e27cce7a480ac16bbcbdffc84f1
uvan1603 <105769785+uvan1603@users.noreply.github.com>
2023-01-09 03:53:39 +0000

    Add files via upload

---
+++ react-redux/src/reducers/ProductsReducer.js
@@ -0,0 +1,12 @@
+import { defaultState } from "./defaultState";
+
+export const productsReducer = (state=defaultState,action)=>{
+    switch (action.type) {
+        case "value":
+            
+            break;
+    
+        default:
+            return state
+    }
+}
