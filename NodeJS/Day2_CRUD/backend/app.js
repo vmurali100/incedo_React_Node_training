4a0eadb4f54e8ab654767872be368925ee0349f2
ChVManjunadhaReddy <chilakala.reddy@incedoinc.com>
2023-01-11 10:04:51 +0530

    Crud with node & Redux toolkit

--- NodeJS/Day2_CRUD/backend/app.js
+++ NodeJS/Day2_CRUD/backend/app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var productsRouter = require('./routes/products')
 var app = express();
+var cors = require('cors')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.options('*', cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- NodeJS/Day2_CRUD/backend/routes/users.js
+++ NodeJS/Day2_CRUD/backend/routes/users.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var router = express.Router();
 var fs = require("fs");
+var jwt = require('jsonwebtoken');
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/", async function (req, res, next) {
+  var users = await getLatestUsers();
+  res.send(users);
 });
 
 router.post("/register", async (req, res) => {
@@ -24,12 +26,46 @@ router.post("/login", async (req, res) => {
   var user = req.body;
   var users = await getLatestUsers();
   var checkUser = isUserExist(users, user);
+  var token = jwt.sign(user, 'some secrete word');
+ 
   if (checkUser) {
-    res.status(200).json({ msg: "login Success !!" });
+    res.status(200).json({ msg: "login Success !!",token });
   } else {
     res.send("Login Failure  . Please try Again !! ");
   }
 });
+
+router.delete("/delete/:email",async (req,res)=>{
+  var userEmail = req.params.email
+  var users = await getLatestUsers();
+  var deletedUsers = users.filter((usr)=> usr.email !== userEmail);
+  var allUsers = await addUsers(deletedUsers);
+  res.send(allUsers)
+})
+
+router.put('/update/:email',async(req,res)=>{
+  var userEmail = req.params.email;
+  var user = req.body
+  var users = await getLatestUsers();
+  console.log(user);
+  users.forEach((usr,i)=>{
+    if(usr['email']==userEmail){
+      users[i]=user
+    }
+  })
+  var allUsers = await addUsers(users);
+  res.send(allUsers)
+
+})
+
+router.post('/createUser',async (req,res)=>{
+  var user = req.body
+  var users = await getLatestUsers();
+  users.push(user);
+  var allUsers = await addUsers(users);
+  res.send(allUsers)
+})
+
 // fs.readFile("./data/users.json", (err, data) => {
 //   var users = JSON.parse(Buffer.from(data).toString());
 //   var checkUser = isUserExist(users, user);
